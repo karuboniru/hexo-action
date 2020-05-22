687c4a3feb845446217c8a8db24a768810ce9ad5
Karuboniru <yanqiyu01@gmail.com>
2020-05-22 21:25:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 
+mkdir ~/tools -p
+curl -s -L https://github.com/jgm/pandoc/releases/download/2.9.2.1/pandoc-2.9.2.1-linux-amd64.tar.gz | tar xvzf - -C ~/tools
+export PATH="$PATH:~/tools/pandoc-2.9.2.1/bin"
 # setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
@@ -17,6 +20,7 @@ npm install hexo-cli -g
 npm install hexo-deployer-git --save
 
 git clone https://github.com/$GITHUB_ACTOR/$GITHUB_ACTOR.github.io.git .deploy_git
+
 
 # deployment
 if [ "$INPUT_COMMIT_MSG" == "" ]
